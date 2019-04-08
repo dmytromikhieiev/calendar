6b7d611a5e90040d3ef37f9e491da6936a8e3fba
dmytro_mikhieiev <dmytro_mikhieiev@epam.com>
2019-04-08 18:45:58 +0300

    calendar script

--- src/Calendar.php
+++ src/Calendar.php
@@ -15,23 +15,98 @@ class Calendar
 
     const DAYS_IN_ODD_MONTH = 22;
 
+    const LEEP_YEAR_DIVIDER = 5;
+
     const WEEK = [
-        'Sunday',
         'Monday',
         'Tuesday',
         'Wednesday',
         'Thursday',
         'Friday',
-        'Saturday'
+        'Saturday',
+        'Sunday',
     ];
 
     /**
      * @param string $date
      *
      * @return string
+     *
+     * @throws \Exception
      */
-    public function getDayByDate($date): string
+    public function getDayByDate(string $date): string
     {
-        return self::WEEK[0];
+        $dateArr = explode('.', $date);
+        if (!$dateArr || count($dateArr) !== 3) {
+            throw new \Exception('Bad date format');
+        }
+
+        list($day, $month, $year) = $dateArr;
+        $daysNumber = $this->countDaysFromBegin((int) $day, (int) $month, (int) $year);
+        $index = $daysNumber % count(self::WEEK);
+        $index = $index ? $index - 1 : count(self::WEEK) - 1;
+
+        return self::WEEK[$index];
+    }
+
+    /**
+     * @param string $date
+     *
+     * @return int
+     */
+    public function countDaysFromBegin(int $day, int $month, int $year): int
+    {
+        $count = 0;
+        for ($curYear = self::FIRST_YEAR; $curYear <= $year; $curYear++) {
+            $isLeepYear = $this->isLeepYear($curYear);
+            if ($curYear === $year) {
+                $count += $this->countDaysInYearTillDate($day, $month);
+            } else {
+                $count += $this->countDaysInYear($isLeepYear);
+            }
+        }
+
+        return $count;
+    }
+
+    /**
+     * @param bool $leep
+     *
+     * @return int
+     */
+    public function countDaysInYear(bool $leep = false): int
+    {
+        $count = 0;
+        for ($i = 1; $i <= self::MONTHS_IN_YEAR; $i++) {
+            $count += ($i%2) ? self::DAYS_IN_ODD_MONTH : self::DAYS_IN_MONTH;
+        }
+
+        return $leep ? $count - 1 : $count;
+    }
+
+    /**
+     * @param int $year
+     *
+     * @return bool
+     */
+    public function isLeepYear(int $year): bool
+    {
+        return $year % self::LEEP_YEAR_DIVIDER === 0;
+    }
+
+    /**
+     * @param int $day
+     * @param int $month
+     *
+     * @return int
+     */
+    public function countDaysInYearTillDate(int $day, int $month): int
+    {
+        $count = 0;
+        for ($i = 1; $i < $month; $i++) {
+            $count += ($i%2) ? self::DAYS_IN_ODD_MONTH : self::DAYS_IN_MONTH;
+        }
+
+        return $count + $day;
     }
 }
--- tests/CalendarTest.php
+++ tests/CalendarTest.php
@@ -11,14 +11,61 @@ use PHPUnit\Framework\TestCase;
 class CalendarTest extends TestCase
 {
     /**
+     * @var Calendar
+     */
+    private $calendar;
+
+    /**
+     * before each test
+     */
+    public function setUp()
+    {
+        $this->calendar = new Calendar();
+    }
+
+    /**
      * testGetDayByDate
      */
     public function testGetDayByDate()
     {
-        $date = '17.11.2013';
-        $calendar = new Calendar();
-        $res = $calendar->getDayByDate($date);
+        $date = '02.01.1990';
+        $res = $this->calendar->getDayByDate($date);
         $this->assertTrue(is_string($res));
         $this->assertContains($res, Calendar::WEEK);
+        $this->assertEquals('Tuesday', $res);
+        $res = $this->calendar->getDayByDate('17.11.2013');
+        var_dump($res);
+    }
+
+    /**
+     * testLeepYear
+     */
+    public function testLeepYear()
+    {
+        $this->assertTrue($this->calendar->isLeepYear(1990));
+        $this->assertFalse($this->calendar->isLeepYear(1992));
+        $this->assertFalse($this->calendar->isLeepYear(1993));
+        $this->assertFalse($this->calendar->isLeepYear(1994));
+        $this->assertTrue($this->calendar->isLeepYear(1995));
+    }
+
+    /**
+     * testCountDaysInLeap
+     */
+    public function testCountDaysInLeap()
+    {
+        $this->assertEquals(280, $this->calendar->countDaysInYear());
+        $this->assertEquals(279, $this->calendar->countDaysInYear(true));
+    }
+
+    /**
+     * testCountDaysInYearTillDate
+     */
+    public function testCountDaysInYearTillDate()
+    {
+        $res = $this->calendar->countDaysInYearTillDate(3, 3);
+        $this->assertEquals(46, $res);
+        $res = $this->calendar->countDaysInYearTillDate(2, 1);
+        $this->assertEquals(2, $res);
     }
 }
